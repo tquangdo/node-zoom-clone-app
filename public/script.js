f161490e37b2ab9d27c41bfdabe69c557722fe5e
tquangdo <trangia61@gmail.com>
2020-09-27 03:01:20 +0900

    20200927 3:01

--- public/script.js
+++ public/script.js
@@ -1,29 +1,147 @@
+const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-// const myPeer = new Peer(undefined, {
-//     path: '/peerjs',
-//     host: '/',
-//     port: '443'
-// })
+//room.ejs > <script src="https://unpkg.com/peerjs@1.3.1/dist/peerjs.min.js">
+const myPeer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '443'
+})
+// var myPeer = new Peer('someid', { secure: true, host: 'your-app-name.herokuapp.com', port: 443, })
 const myVideo = document.createElement('video')
-myVideo.muted = true;
-let myVideoStream;
+// myVideo.muted = true
+let myVideoStream
+const peers = {}
+
+// ---------------- MAIN
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
-}).then(stream => {
-    myVideoStream = stream;
-    addVideoStream(myVideo, stream)
-    // myPeer.on('call', call => {
-    //     call.answer(stream)
-    //     const video = document.createElement('video')
-    //     call.on('stream', userVideoStream => {
-    //         addVideoStream(video, userVideoStream)
-    //     })
-})
-const addVideoStream = (video, stream) => {
-    video.srcObject = stream
-    video.addEventListener('loadedmetadata', () => {
-        video.play()
+}).then(item_stream => {
+    myVideoStream = item_stream
+    addVideoStream(myVideo, item_stream)
+    myPeer.on('call', call => {
+        //★★★ 6) call.answer()
+        call.answer(item_stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
     })
-    videoGrid.append(video)
+    //★★★ 4)
+    socket.on('S_USER_KETNOIOK_1', arg_userId => {
+        setTimeout(function () { onConnectToNewUser(arg_userId, item_stream) }, 1000)
+    })
+
+    // input value
+    let text = $("input")
+    // when press enter send message
+    $('html').keydown(function (e) {
+        if (e.which == 13 && text.val().length !== 0) {
+            //★★★ 7)
+            socket.emit('C_MSG_2', text.val())
+            text.val('')
+        }
+    })
+    //★★★ 10)
+    socket.on("S_GUI_MSG_ALL_2", (argMessage, argUserId) => {
+        $("ul").append(`<li class="message"><b>user [${argUserId}]: </b><br/>${argMessage}</li>`)
+        scrollToBottom()
+    })
+})
+// ---------------- MAIN
+
+socket.on('S_USER_OUT_3', userId => {
+    if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', arg_id => {
+    //truyền từ room.ejs: "ROOM_ID" = xxx
+    //phải call emit "C_VOROOM_1" trong myPeer.on() thì socket.on("C_VOROOM_1") mới nhận được (userId)
+    //★★★ 2)
+    socket.emit('C_VOROOM_1', ROOM_ID, arg_id)
+})
+
+const onConnectToNewUser = (arg_userId, arg_stream) => {
+    const call = myPeer.call(arg_userId, arg_stream)
+    const video = document.createElement('video')
+    //★★★ 5) call.on()
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+
+    peers[arg_userId] = call
 }
+
+const addVideoStream = (arg_video, arg_stream) => {
+    arg_video.srcObject = arg_stream
+    arg_video.addEventListener('loadedmetadata', () => {
+        arg_video.play()
+    })
+    videoGrid.append(arg_video)
+}
+
+// ---------------- SUB
+const scrollToBottom = () => {
+    var d = $('.main__chat_window');
+    d.scrollTop(d.prop("scrollHeight"));
+}
+
+
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    } else {
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+const playStop = () => {
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo()
+    } else {
+        setStopVideo()
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+
+const setMuteButton = () => {
+    const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+    const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setStopVideo = () => {
+    const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `
+    document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+    const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `
+    document.querySelector('.main__video_button').innerHTML = html;
+}
+// ---------------- SUB
+
--- server.js
+++ server.js
@@ -1,25 +1,44 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
-app.use(cors())
+// const cors = require('cors')
+// app.use(cors())
 const server = require('http').Server(app)
-// const io = require('socket.io')(server)
-// const { ExpressPeerServer } = require('peer');
-// const peerServer = ExpressPeerServer(server, {
-//     debug: true
-// });
-// app.use('/peerjs', peerServer);
+const io = require('socket.io')(server)
+const { ExpressPeerServer } = require("peer")
+const peerServer = ExpressPeerServer(server, {
+    // debug: true,
+})
+app.use('/peerjs', peerServer) //kết hợp peer & peerjs
 const { v4: uuidV4 } = require('uuid')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.redirect(`/${uuidV4()}`)
+    res.redirect(`/${uuidV4()}`) //run "localhost:9000" sẽ redirect sang "localhost:9000/xxx"
 })
 
 app.get('/:room', (req, res) => {
-    res.render('room', { roomId: req.params.room })
+    res.render('room', { roomId: req.params.room }) //khi redirect thì truyền biến "roomId"=xxx
+})
+io.on('connection', socket => {
+    socket.on('C_VOROOM_1', (roomId, userId) => {
+        socket.userId = userId
+        //★★★ 3)
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('S_USER_KETNOIOK_1', userId)
+        //★★★ 8)
+        socket.on('C_MSG_2', (arg_message) => {
+            //send arg_message to the same room
+            //★★★ 9)
+            io.to(roomId).emit('S_GUI_MSG_ALL_2', arg_message, socket.userId)
+        })
+
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('S_USER_OUT_3', userId)
+        })
+    })
 })
 
-server.listen(process.env.PORT || 1235, () => console.log('Server khoi dong OK!'))
+//★★★ 1)
+server.listen(process.env.PORT || 443, () => console.log('Server khoi dong OK!'))
